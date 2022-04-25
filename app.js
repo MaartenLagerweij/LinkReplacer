46e1865ae960193967539a51d287d9893a914e8c
Maarten Lagerweij <maarten.lagerweij1@gmail.com>
2022-04-25 10:08:23 +0200

    Add .csv upload functionality & convert data into dataObject object

--- app.js
+++ app.js
@@ -1,5 +1,32 @@
+let selectedFile,data;
+const dataObject = {}
+
+document.getElementById('excel-input').addEventListener('change', (event)=> {
+    selectedFile = event.target.files[0];
+})
+
+document.getElementById('excel-button').addEventListener('click', ()=> {
+    if(selectedFile){
+        let fileReader = new FileReader();
+        fileReader.readAsBinaryString(selectedFile);
+        fileReader.onload = (event)=> {
+            data = event.target.result
+            dataArray = data.split(/\r?\n/);
+            dataArray.forEach(element => {
+                let arr = element.split(',')
+                //This will save everything from column A in the .csv as a property and column B as the value:
+                dataObject[arr[0]] = arr[1]
+            })
+        }
+    }
+})
+
+
+
 function myFunction(){
     const str = document.getElementById('text-input').value
     document.getElementById('output').innerHTML = str
     
+    
+
 }
